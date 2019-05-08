3a3406b488875a3687038d351cbd76f67bda1810
Carla <morillocarla7@gmail.com>
2019-05-08 12:17:22 +0200

    Campo de juego creado

---
+++ runners.py
@@ -0,0 +1,33 @@
+import pygame, sys
+
+class Game():
+    runners = []
+    __startLine = 20
+    __finishLine = 620
+    
+#    Constructor
+    def __init__(self):
+        self.__screen = pygame.display.set_mode((640,480))
+        self.__screen.fill((192, 57, 43))
+#        self.__background = pygame.image.load('imagenes/background.png')
+        pygame.display.set_caption('Carrera de bichos')
+        
+    
+    def competir(self):
+        gameOver = False
+        while not gameOver:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameOver = True
+        
+#            self.__screen.blit(self.__backgroun, (0,0))
+            pygame.display.flip()
+        
+        pygame.quit()
+        sys.exit()
+        
+    
+if __name__ == '__main__':
+    game = Game()
+    pygame.font.init()
+    game.competir()
